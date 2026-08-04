47d247159edb1af805123bee1fb8ef65e69b19cf
ErwinBezu <erwin.bezu@gmail.com>
2026-08-04 17:01:32 +0200

    eleventh question: add int_employee_stats

--- northwind_dwh/models/intermediate/int_customers_stats.sql
+++ northwind_dwh/models/intermediate/int_customers_stats.sql
@@ -13,7 +13,7 @@ WITH commandes_clients AS (
 SELECT
     customer_id,
     COUNT(*) AS nb_commandes, 
-    ROUND(SUM(montant_total),2) AS ca_total, 
+    ROUND(SUM(montant_total), 2) AS ca_total, 
     MIN(order_date) AS date_premiere_commande, 
     MAX(order_date) AS date_derniere_commande, 
     ROUND(AVG(order_date - date_commande_precedente), 2) AS delai_moyen_entre_commandes

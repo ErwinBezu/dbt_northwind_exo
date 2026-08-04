e7de0ff641c97c48e18ef53cdfd28fa40ed9f28a
ErwinBezu <erwin.bezu@gmail.com>
2026-08-04 10:30:39 +0200

    third question: add stg_orders.sql

---
+++ northwind_dwh/models/staging/stg_customers.sql
@@ -0,0 +1,9 @@
+SELECT 
+    customer_id, 
+    TRIM(company_name) AS company_name,
+    INITCAP(TRIM(contact_name)) AS contact_name,
+    INITCAP(TRIM(contact_title)) AS contact_title,
+    INITCAP(TRIM(city)) AS city,
+    INITCAP(TRIM(country)) AS country, 
+    TRIM(phone) AS phone
+FROM {{ source('northwind', 'customers') }};

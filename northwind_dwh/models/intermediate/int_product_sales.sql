18468fb4729fb1e1547e2c72f31ac16e598225d9
ErwinBezu <erwin.bezu@gmail.com>
2026-08-05 09:40:29 +0200

    thirteenth question: add int_product_sales.sql

---
+++ northwind_dwh/models/intermediate/int_product_sales.sql
@@ -0,0 +1,12 @@
+SELECT
+    p.product_id,
+    COALESCE(SUM(od.quantity), 0) AS quantite_totale_vendue, 
+    COALESCE(ROUND(SUM(od.sous_total), 2), 0) AS ca_genere, 
+    COUNT(DISTINCT od.order_id) AS nb_commandes_distinctes, 
+    p.units_in_stock AS stock_restant
+FROM {{ ref('stg_products') }} p
+LEFT JOIN {{ ref('stg_order_details') }} od
+    ON p.product_id = od.product_id
+GROUP BY 
+    p.product_id,
+    p.units_in_stock

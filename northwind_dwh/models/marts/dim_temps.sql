dd23ccdaa8e4367f859a12c3f53486188077fc38
ErwinBezu <erwin.bezu@gmail.com>
2026-08-05 11:49:27 +0200

    seventeenth question: add dim_temps.sql

---
+++ northwind_dwh/models/marts/dim_temps.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT
+    order_date AS date_id,
+    EXTRACT(DAY FROM order_date) AS jour, 
+    EXTRACT(MONTH FROM order_date) AS mois, 
+    EXTRACT(YEAR FROM order_date) AS année,
+    EXTRACT(QUARTER FROM order_date) AS trimestre,
+    TO_CHAR(order_date, 'YYYY-MM') AS annee_mois, 
+    (EXTRACT(DOW FROM order_date) = 0 OR EXTRACT(DOW FROM order_date) = 6) AS est_weekend
+FROM {{ ref('stg_orders')}}     
